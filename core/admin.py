7b5eaf938e42f477bce4d7cb8931425092ea4469
avadheshy <avadheshy2015@gmail.com>
2022-07-16 23:34:43 +0530

    initial commit

---
+++ core/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from core.models import Like,Comment,Post,Follow
+# Register your models here.
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Post)
+admin.site.register(Follow)
---
+++ user/admin.py
@@ -0,0 +1,6 @@
+from re import U
+from django.contrib import admin
+from user.models import User
+
+# Register your models here.
+admin.site.register(User)
