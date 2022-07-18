c13b2302843d6f1d6f64ec3fc7608ab77a1af6ca
avadheshy <avadheshy2015@gmail.com>
2022-07-18 14:15:08 +0530

    jkx

--- instagram/settings.py
+++ instagram/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-nu2s85q!#psx8_%7vykjh^)q-_*5ema75y5k-$+z#8m+h3q!6w'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS=[config('ALLOWED_HOSTS')]
