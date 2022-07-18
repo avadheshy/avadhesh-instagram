3670aeb6e09c69542c8f348fabdac5735e2113e0
avadheshy <avadheshy2015@gmail.com>
2022-07-18 13:50:21 +0530

    post added

--- instagram/settings.py
+++ instagram/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-#ALLOWED_HOSTS=[config('ALLOWED_HOSTS')]
+ALLOWED_HOSTS=[config('ALLOWED_HOSTS')]
 #ALLOWED_HOSTS = ['avadhesh-insta.herokuapp.com']
-ALLOWED_HOSTS=[]
+#ALLOWED_HOSTS=[]
 
 
 # Application definition
