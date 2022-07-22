57b979cb6d6d5723b41b03f6554f598495e6344c
avadheshy <avadheshy2015@gmail.com>
2022-07-22 07:36:02 +0530

    follower added

--- core/models.py
+++ core/models.py
@@ -7,17 +7,23 @@ User = get_user_model()
 
 
 # Posts Model
-class Post(models.Model): 
-    text = models.CharField(max_length=140, blank=True, null=True,)  # null == None, blank = ''
-    img = models.ImageField(upload_to='post_images/')   # BASE_DIR -> media -> post_images
-    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='user_user')    # user_id
+class Post(models.Model):
+    text = models.CharField(max_length=140, blank=True,
+                            null=True,)  # null == None, blank = ''
+    # BASE_DIR -> media -> post_images
+    img = models.ImageField(upload_to='post_images/')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_user')    # user_id
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    liked=models.ManyToManyField(User,default=0,blank=True,related_name='user_liked')
+    liked = models.ManyToManyField(
+        User, default=0, blank=True, related_name='user_liked')
+
     @property
     def num_like(self):
         return self.liked.all().count()
-   
+
+
 class Comment(models.Model):
     text = models.CharField(max_length=240)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -28,39 +34,33 @@ class Comment(models.Model):
     def __str__(self):
         return f'{self.text} by {self.user}'
 
-    
 
 # Likes Model
 class Like(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name='posts')
-    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='users')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='posts')
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='users')
     liked_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.post.id)
+
     @property
     def likes_count(self):
         count = self.like_set.count()
         return count
 
-    
 
 # Followers Model
 class Follow(models.Model):
-    user = models.ForeignKey(User, related_name='follow_follower', 
-    on_delete=models.CASCADE)
-    followed = models.ForeignKey(User, related_name='follow_followed', 
-    on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='follow_follower',
+                             on_delete=models.CASCADE)
+    followed = models.ForeignKey(User, related_name='follow_followed',
+                                 on_delete=models.CASCADE)
     followed_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return f"{self.user} {self.followed}"
-    
-
-    
-
-
-
