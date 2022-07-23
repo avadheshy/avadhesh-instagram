2945d7c134f71084ab3372191e3f848b70712eae
avadheshy <avadheshy2015@gmail.com>
2022-07-23 12:58:36 +0530

    followers added

--- user/urls.py
+++ user/urls.py
@@ -8,11 +8,14 @@ urlpatterns = [
     path('posts', views.posts, name='posts'),
     path('profile', views.profile, name='profile'),
     path('logout', views.logout, name='logout'),
-    path('comments/<int:post_id>',views.comments,name='comments'),
-    path('likes/<int:like_id>',views.likes,name='likes'),
-    path('follow/<int:post_user_id>',views.follow,name='follow'),
-    path('otherProfile/<int:otherProfile_id>',views.otherProfile,name='otherProfile'),
-    path('otherProfile/unfollow/<int:unfollow_id>',views.unfollow,name='unfollow'),
-
+    path('comments/<int:post_id>', views.comments, name='comments'),
+    path('likes/<int:like_id>', views.likes, name='likes'),
+    path('follow/<int:post_user_id>', views.follow, name='follow'),
+    path('otherProfile/<int:otherProfile_id>',
+         views.otherProfile, name='otherProfile'),
+    path('otherProfile/unfollow/<int:unfollow_id>',
+         views.unfollow, name='unfollow'),
+    path('followers', views.followers, name='followers'),
+    path('following', views.following, name='following'),
 ]
 #urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/views.py
+++ user/views.py
@@ -112,29 +112,59 @@ def follow(request, post_user_id):
         new_follow = Follow(user=user_obj, followed=post_user_obj)
         new_follow.save()
         return redirect('login')
-def otherProfile(request,otherProfile_id):
-    if request.method=='POST':
-        otherUser=User.objects.get(id=otherProfile_id)
-        allPosts=Post.objects.filter(user=otherUser)
+
+
+def otherProfile(request, otherProfile_id):
+    if request.method == 'POST':
+        otherUser = User.objects.get(id=otherProfile_id)
+        allPosts = Post.objects.filter(user=otherUser)
         following_count = Follow.objects.filter(user=otherUser).count()
         followers_count = Follow.objects.filter(followed=otherUser).count()
-        postCount=len(allPosts)
-        context={
-            'allPost':allPosts,
-            'follow_count':following_count,
-            'followers_count':followers_count,
-            'otherProfile_id':otherProfile_id,
-            'otherUser':otherUser,
-            'postCount':postCount
-           
+        postCount = len(allPosts)
+        context = {
+            'allPost': allPosts,
+            'follow_count': following_count,
+            'followers_count': followers_count,
+            'otherProfile_id': otherProfile_id,
+            'otherUser': otherUser,
+            'postCount': postCount
+
         }
         return render(request, 'user/userProfile.html', context)
 
-def unfollow(request,unfollow_id):
-    if request.method=='POST':
-        followedUser=User.objects.filter(id=unfollow_id)
+
+def unfollow(request, unfollow_id):
+    if request.method == 'POST':
+        followedUser = User.objects.filter(id=unfollow_id)
         print('hello')
-        del_follower=Follow.objects.filter(user_id=request.user.id).filter(followed_id=unfollow_id)
-    
+        del_follower = Follow.objects.filter(
+            user_id=request.user.id).filter(followed_id=unfollow_id)
+
         del_follower.delete()
         return redirect('login')
+
+
+def followers(request):
+    if request.method == 'POST':
+        people = Follow.objects.filter(followed=request.user).values('user_id')
+        following_id = []
+        for person in people:
+            following_id.append(person['user_id'])
+        following = User.objects.filter(id__in=following_id)
+        yourFollowers = []
+        for people in following:
+            yourFollowers.append(people)
+        return render(request, 'user/followers.html', {'yourFollowers': yourFollowers})
+
+
+def following(request):
+    if request.method == 'POST':
+        people = Follow.objects.filter(user=request.user).values('followed_id')
+        people_id = []
+        for per in people:
+            people_id.append(per['followed_id'])
+        ans = User.objects.filter(id__in=people_id)
+        people_follow = []
+        for abc in ans:
+            people_follow.append(abc.username)
+        return render(request, 'user/following.html', {'people_follow': people_follow})
